deb09aa2d9c08339955ecec801d323704bb59459
borisloor06 <boris.loor06@gmail.com>
2024-05-09 00:33:45 -0500

    chore: Add TypeORM and PostgreSQL support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthResolver } from './auth/auth.resolver';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService, AuthResolver],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASS'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: false,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
