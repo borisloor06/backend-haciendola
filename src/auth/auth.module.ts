97922b51e88f31394890ccdae3e6553ef9b4ad3b
borisloor06 <boris.loor06@gmail.com>
2024-05-13 00:29:44 -0500

    feature: Add jwt auth and guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auth } from './entities/auth.entity';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auth])],
+  imports: [UserModule, TypeOrmModule.forFeature([Auth])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [PassportModule, AuthService, LocalStrategy],
 })
 export class AuthModule {}
