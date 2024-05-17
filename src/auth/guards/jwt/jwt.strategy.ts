8f2a1c0f6b3e1eb4bade8e08a367a23fcc6907f3
borisloor06 <boris.loor06@gmail.com>
2024-05-17 06:18:38 -0500

    chore: Remove unnecessary log statement in JwtStrategy

--- src/auth/guards/jwt/jwt.strategy.ts
+++ src/auth/guards/jwt/jwt.strategy.ts
@@ -14,7 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    Logger.log(payload);
     return { userId: payload.sub, username: payload.username };
   }
 }
