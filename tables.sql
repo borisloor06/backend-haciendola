3ca793ae0094bc0d217b392dd936273b54c1153c
borisloor06 <boris.loor06@gmail.com>
2024-05-17 06:33:45 -0500

    feat: Add product table with necessary columns

--- tables.sql
+++ tables.sql
@@ -4,3 +4,16 @@ create table "user" (
     "password" character varying NOT NULL,
     "email" character varying NOT NULL
 );
+
+create table "product" (
+    "id" SERIAL PRIMARY KEY,
+    "handle" character varying NOT NULL,
+    "title" character varying NOT NULL,
+    "description" character varying NOT NULL,
+    "sku" character varying NOT NULL,
+    "grams" integer NOT NULL,
+    "stock" integer NOT NULL,
+    "price" numeric NOT NULL,
+    "compare_price" numeric NOT NULL,
+    "barcode" character varying NOT NULL
+);
